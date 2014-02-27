0233608abd40f620d43ea4b862f83f0b98ed42bb
Tim Smart <tim@fostle.com>
2014-02-27 16:11:41 +1300

    README and dev commands
    Signed-off-by: Tim Smart <tim@fostle.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,14 @@
 module.exports = function (grunt) {
   grunt.initConfig(
     { pkg         : grunt.file.readJSON('package.json')
+    , concurrent  :
+      { dev       :
+        { tasks   : ['watch', 'nodemon']
+        , options :
+          { logConcurrentOutput : true
+          }
+        }
+      }
     , coffee      :
       { app       :
         { options :
@@ -13,6 +21,16 @@ module.exports = function (grunt) {
         , ext     : '.js'
         }
       }
+    , nodemon     :
+      { dev       :
+        { script  : 'server.js'
+        , options :
+          { ext   : 'js'
+          , watch : ['app']
+          , delay : 1
+          }
+        }
+      }
     , watch       :
       { coffee    :
         { files   : ['app-src/**/*.coffee']
@@ -22,8 +40,11 @@ module.exports = function (grunt) {
     }
   )
 
+  grunt.loadNpmTasks('grunt-concurrent')
   grunt.loadNpmTasks('grunt-contrib-coffee')
   grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-nodemon')
 
   grunt.registerTask('default', ['coffee'])
+  grunt.registerTask('start-dev-server', ['default', 'concurrent:dev'])
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+# dplyr
+
+Musical democracy for Design Works.
+
+
+## Getting started
+
+    $ npm install && grunt start-dev-server
+
+Then open http://127.0.0.1:8080/ in your web browser.
