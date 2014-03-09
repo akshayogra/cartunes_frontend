be47329a9afab8a6cefaa2bc665a912ca5196435
Tim Smart <tim@fostle.com>
2014-03-10 12:36:01 +1300

    Remove un-needed files
    Signed-off-by: Tim Smart <tim@fostle.com>

--- app/app-src/config/database.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var redis;
-
-redis = require('redis');
-
-module.exports = function(app) {
-  return app.set('redis', redis.createClient(app.set('redis port'), app.set('redis host')));
-};
