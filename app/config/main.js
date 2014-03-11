29e7bec13a61503515bb64cf7e44669f72866360
Tim Smart <tim@fostle.com>
2014-03-12 09:15:01 +1300

    Update app/
    Signed-off-by: Tim Smart <tim@fostle.com>

--- app/config/main.js
+++ app/config/main.js
@@ -9,10 +9,8 @@ mpath = require('path');
 module.exports = function(app) {
   app.set('views', mpath.resolve(mpath.join(__dirname, '../../app-src/views')));
   app.set('public', mpath.resolve(mpath.join(__dirname, '../public')));
-  app.use(express.logger());
-  app.use(express.json());
-  app.use(express.urlencoded());
   app.set('view engine', 'jade');
+  app.use(express.logger());
   app.use(express.compress());
   return app.use(express["static"](app.set('public')));
 };
