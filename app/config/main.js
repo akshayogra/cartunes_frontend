793144e272181c07535f69ef6a8fd155dfb9d89b
Tim Smart <tim@fostle.com>
2014-03-13 08:51:42 +1300

    Update app
    Signed-off-by: Tim Smart <tim@fostle.com>

--- app/config/main.js
+++ app/config/main.js
@@ -7,9 +7,7 @@ express = require('express');
 mpath = require('path');
 
 module.exports = function(app) {
-  app.set('views', mpath.resolve(mpath.join(__dirname, '../../app-src/views')));
   app.set('public', mpath.resolve(mpath.join(__dirname, '../public')));
-  app.set('view engine', 'jade');
   app.use(express.logger());
   app.use(express.compress());
   return app.use(express["static"](app.set('public')));
